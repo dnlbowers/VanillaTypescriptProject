3aacbcfc431f7bbfa8cefb6b42542446c4cdf908
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-09-12 23:31:28 +0200

    Rename name scriptLoader.ts and add docstring

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import {loadScript} from "./utils/script-loader.js";
+import {loadScript} from "./utils/scriptLoader.js";
 
 const componentRef: NodeListOf<Element> = document.querySelectorAll('[data-component]');
+
 document.addEventListener("DOMContentLoaded", function () {
+    /*
+    * Cycles through the data-component elements and injects the scripts, style and html
+     */
     componentRef.forEach((element: Element) => {
-        const component = element.getAttribute('data-component');
+        const component :string | null  = element.getAttribute('data-component');
         console.log(component)
         const fileName = `components/${component}/${component}`
         loadScript(fileName)
