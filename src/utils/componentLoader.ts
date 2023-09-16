30a0d6dd4ad1588f4ed4e705b3e8e7c24e372c4d
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-09-17 01:32:19 +0200

    Update componentLoader to inject html

--- src/utils/componentLoader.ts
+++ src/utils/componentLoader.ts
@@ -8,23 +8,43 @@ export class ComponentLoader {
         this.url = url;
         this.loadScript();
         this.loadCss();
+        this.loadHtml();
     }
     private loadScript() {
         const scriptTag :HTMLScriptElement = document.createElement('script');
         scriptTag.type = 'module';
         scriptTag.src = `js/${this.url}.js`;
         document.head.appendChild(scriptTag);
+        console.log(scriptTag.src)
     }
 
     private loadCss() {
         const linkTag: HTMLLinkElement = document.createElement('link');
-        linkTag.rel = 'stylesheet'
-        linkTag.href = `css/${this.url}.css`
-        document.head.appendChild(linkTag)
+        linkTag.rel = 'stylesheet';
+        linkTag.href = `css/${this.url}.css`;
+        console.log(linkTag.href)
+        document.head.appendChild(linkTag);
+    }
+
+    private loadHtml() {
+        // Determine the element that will host the HTML content.
+        // This is based on the URL, which should be something like "components/header/header".
+        const componentName = this.url.split('/')[1];  // This should be like "header"
+        const hostElement = document.querySelector(`app-${componentName}`);
+
+        // Only proceed if the host element exists
+        if (hostElement) {
+            const xhr = new XMLHttpRequest();
+            xhr.open('GET', `${this.url}.html`, true);
+
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    hostElement.innerHTML = xhr.responseText;
+                }
+            };
+
+            xhr.send();
+        }
     }
 }
-
-
-
-
 
