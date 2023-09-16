7871dbfc622f3155a74361a0dea8cf5de76a8008
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-09-17 01:35:13 +0200

    refactor: Use custom HTML tags in place of data attribute

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import {ComponentLoader} from "./utils/componentLoader.js";
 
-const componentRef: NodeListOf<Element> = document.querySelectorAll('[data-component]');
+const allElements: HTMLCollectionOf<Element> = document.getElementsByTagName('*');
+const appElements: Element[] = Array.from(allElements).filter(el => el.tagName.toLowerCase().startsWith('app-'));
 
 document.addEventListener("DOMContentLoaded", function () {
     /*
-    * Cycles through the data-component elements and injects the scripts, style and html
-     */
-    componentRef.forEach((element: Element) => {
-        const component :string | null  = element.getAttribute('data-component');
-        console.log(component)
-        const fileName = `components/${component}/${component}`
+    * Cycles through the custom app-* elements and injects the scripts, style, and html
+    */
+    appElements.forEach((element: Element) => {
+        const component: string = element.tagName.toLowerCase();  // Now it's like "app-header"
+        const componentName: string = component.substring(4);  // Remove "app-" prefix, resulting in "header"
+        console.log(componentName);
+        const fileName = `components/${componentName}/${componentName}`;
         new ComponentLoader(fileName);
     });
 });
